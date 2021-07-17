6362d26f0e2587616fda58ea42f643133acbe537
uday kumar <65162484+uday-vv@users.noreply.github.com>
2021-07-17 21:36:30 +0530

    Add files via upload

---
+++ src/pages/Css.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Css() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Css
---
+++ src/pages/Js.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Js() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Js
